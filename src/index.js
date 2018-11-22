27d93b114b8f7181cc2a594edf223c069532f315
AlexisPescoran <alexisfer.18@gmail.com>
2018-11-22 12:30:12 -0500

    function code and function encode are done in cipher.js

--- src/index.js
+++ src/index.js
@@ -6,33 +6,31 @@ const txtText1 = document.getElementById('txt-text1');
 const txtText2 = document.getElementById('txt-text2');
 const txtEncode = document.getElementById('txt-encode');
 const txtDecode = document.getElementById('txt-decode');
+const numEncode = document.getElementById('num-encode');
+const numDecode = document.getElementById('num-decode');
 
-
-/*function startProgram(){
-  alert('probando');
-}*/
-
+//Click del botón empezar
 const startProgram = () => {
   alert('probando');
 }
-
+//Click del botón cifrar
 const actionEncode = () => {
   let texto1 = txtText1.value;
-  txtEncode.value = texto1;
-  alert(encode(texto1));
-}
+  let number1 = parseInt(numEncode.value);
 
+  txtEncode.value = cipher.encode(number1,texto1);
+  //alert(cipher.encode(number1,texto1));
+}
+//Click del botón descifrar
 const actionDecode = () => {
   let texto2 = txtText2.value;
-  txtDecode.value = texto2;
+  let number2 = parseInt(numDecode.value);
 
+  txtDecode.value = cipher.decode(number2,texto2);
 }
 
-const encode = (letra) => {
-  let l = letra;
-  return l.toUpperCase();
-}
 
+//Eventos de los botones
 btnStart.addEventListener('click', startProgram);
 btnEncode.addEventListener('click', actionEncode);
 btnDecode.addEventListener('click', actionDecode);
