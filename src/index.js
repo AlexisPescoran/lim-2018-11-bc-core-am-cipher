396bc20924c870138e418f7c9a0f811a46989851
AlexisPescoran <alexisfer.18@gmail.com>
2018-11-27 13:00:08 -0500

    cambio de pantalla cifrar por tema de usuario

--- src/index.js
+++ src/index.js
@@ -9,57 +9,19 @@ const btnEncode = document.getElementById('btn-encode');
 const btnDecode = document.getElementById('btn-decode');
 const btnGoBack1 = document.getElementById('btn-go-back-1');
 const btnGoBack2 = document.getElementById('btn-go-back-2');
-const btnAddType = document.getElementById('btn-add-type');
-const btnAddModel = document.getElementById('btn-add-model');
-const btnAddUnits = document.getElementById('btn-add-units');
-const btnAddCost = document.getElementById('btn-add-cost');
-const btnAddSale = document.getElementById('btn-add-sale');
-const btnShowList = document.getElementById('btn-show-list');
 
-//const txtText1 = document.getElementById('txt-text1');
+
+const txtText1 = document.getElementById('txt-text1');
 const txtText2 = document.getElementById('txt-text2');
 const txtEncode = document.getElementById('txt-encode');
-const txtDecode = document.getElementById('txt-decode');
+//const txtDecode = document.getElementById('txt-decode');
 const numEncode = document.getElementById('num-encode');
 const numDecode = document.getElementById('num-decode');
-const txtType = document.getElementById('txt-type');
-const txtModel = document.getElementById('txt-model');
-const txtUnits = document.getElementById('txt-units');
-const txtCost = document.getElementById('txt-cost');
-const txtSale = document.getElementById('txt-sale');
 
 
-//Variables globales de los arrays
-let arrayType = []; let arrayModel = []; let arrayUnits = []; let arrayCost = []; let arraySale = [];
 //Click del botón ver instrucciones
 const seeInstructions = () => {
   alert('probando');
-}
-
-//Click del boton añadir Tipo
-addType = () =>{
-  arrayType.push(txtType.value);
-  txtType.value = '';
-}
-
-//Clic del botón añadir Modelo
-addModel = () =>{
-  arrayModel.push(txtModel.value);
-  txtModel.value = '';
-}
-
-//Click del botón mostrar lista
-showList = () =>{
-  for(let i=0; i<arrayType.length; i++){
-    txtEncode.value=arrayType[i];
-    console.log(arrayType[i]);
-  }
-  for(let i=0; i<arrayModel.length; i++){
-    txtEncode.value=arrayModel[i];
-    console.log(arrayModel[i]);
-  }
-  txtEncode.value=arrayModel;
-  txtEncode.value=arrayType; //se quedó con lo último
 }
 
 //Click del botón Volver a inicio 1
@@ -68,7 +30,6 @@ goBack1 = () => {
     document.getElementById('screen-1').style.display='block';
     clearInputs();
 }
-
 //Click del botón Volver a inicio 2
 goBack2 = () => {
     document.getElementById('screen-3').style.display='none';
@@ -78,8 +39,10 @@ goBack2 = () => {
 
 //Creando la función limpiar
 clearInputs = () => {
+  txtText1.value="";
   numEncode.value="";
-  txtEncode.value="";
+  //txtEncode.value="";
+  document,getElementById('new').innerHTML="";
 
   txtText2.value="";
   numDecode.value="";
@@ -88,10 +51,11 @@ clearInputs = () => {
 
 //Click del botón cifrar
 const actionEncode = () => {
-  /*let texto1 = txtText1.value.toUpperCase();
+  let texto1 = txtText1.value.toUpperCase();
   let number1 = parseInt(numEncode.value);
 
-  txtEncode.value = cipher.encode(number1,texto1);*/
+  document.getElementById('new').innerHTML = 'Aquí tienes los nombres secretos';
+  txtText1.value = cipher.encode(number1,texto1);
 }
 
 //Click del botón descifrar
@@ -108,12 +72,6 @@ btnEncode.addEventListener('click', actionEncode);
 btnDecode.addEventListener('click', actionDecode);
 btnGoBack1.addEventListener('click', goBack1);
 btnGoBack2.addEventListener('click', goBack2);
-btnAddType.addEventListener('click', addType);
-btnAddModel.addEventListener('click', addModel);
-btnAddUnits.addEventListener('click', addUnits = () =>{alert('probando3');});
-btnAddCost.addEventListener('click', addCost = () =>{alert('probando4');});
-btnAddSale.addEventListener('click', addSale = () =>{alert('probando5');});
-btnShowList.addEventListener('click', showList);
 
 btnGoEncode.addEventListener('click', goEncode = () => {
     document.getElementById('screen-1').style.display='none';
