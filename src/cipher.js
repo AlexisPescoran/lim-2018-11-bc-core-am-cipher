e2d2942557def81a748896f28862234f5566c877
AlexisPescoran <alexisfer.18@gmail.com>
2018-11-28 13:47:11 -0500

    testing and fixing (errors with else)

--- src/cipher.js
+++ src/cipher.js
@@ -1,30 +1,31 @@
 window.cipher = {
   // ...
  encode: (offset, string) => {
-   let mensaje = '';
+   let mensaje = "";
 
   for(let i=0 ; i<string.length ; i++){
+
     if(string[i].charCodeAt() >= 65 && string[i].charCodeAt() <= 90){
       let newAscciPosition = (((string[i].charCodeAt() -65) +offset) %26) +65;
       let newChar = String.fromCharCode(newAscciPosition);
       mensaje = mensaje + newChar;
-    }else {
-      mensaje = mensaje+string[i];
-    }
+    }/*else {
+      mensaje = mensaje + string[i];
+    }*/
   }
    return mensaje;
  },
  decode: (offset, string) => {
-   let mensaje = '';
+   let mensaje = "";
 
     for(let i=0 ; i<string.length ; i++){
      if(string[i].charCodeAt() >= 65 && string[i].charCodeAt() <= 90){
        let newAscciPosition = (((string[i].charCodeAt() -90 ) -offset) %26) +90;
        let newChar = String.fromCharCode(newAscciPosition);
        mensaje = mensaje + newChar;
-     }else {
+     }/*else {
        mensaje = mensaje+string[i];
-     }
+     }*/
    }
    return mensaje;
  }
